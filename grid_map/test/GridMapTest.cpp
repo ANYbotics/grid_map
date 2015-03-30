f8a963ca034d876ddb06b1188777b5aecc80af08
Peter Fankhauser <pfankhauser@ethz.ch>
2015-03-30 19:31:14 +0200

    Fixed unit tests.

--- grid_map/test/GridMapTest.cpp
+++ grid_map/test/GridMapTest.cpp
@@ -6,69 +6,71 @@
  *	 Institute: ETH Zurich, Autonomous Systems Lab
  */
 
-#include "grid_map/GridMap.hpp"
+#include "grid_map_core/GridMap.hpp"
+#include "grid_map/GridMapRosConverter.hpp"
 
 // gtest
 #include <gtest/gtest.h>
 
 // Eigen
 #include <Eigen/Core>
-#include <Eigen/Dense>
+
+// STD
+#include <string>
 
 using namespace std;
-using namespace Eigen;
-using namespace grid_map_lib;
 using namespace grid_map;
 
 TEST(RosbagHandling, saveLoad)
 {
-  Matrix2f dataIn, dataOut;
+  Eigen::Matrix2f dataIn, dataOut;
   dataIn << 1.0, 2.0,
             3.0, 4.0;
-  string type = "type";
-  string bagName = "bagName.bag";
-  string topicName = "topicName";
+  string layer = "layer";
+  string pathToBag = "test.bag";
+  string topic = "topic";
   grid_map::GridMap gridMapIn, gridMapOut;
-  gridMapIn.add(type, dataIn);
+  gridMapIn.add(layer, dataIn);
 
-  EXPECT_FALSE(gridMapOut.exists(type));
+  EXPECT_FALSE(gridMapOut.exists(layer));
 
-  EXPECT_TRUE(gridMapIn.saveToBag(bagName, topicName));
-  EXPECT_TRUE(gridMapOut.loadFromBag(bagName, topicName));
+  EXPECT_TRUE(GridMapRosConverter::saveToBag(gridMapIn, pathToBag, topic));
+  EXPECT_TRUE(GridMapRosConverter::loadFromBag(pathToBag, topic, gridMapOut));
 
-  EXPECT_TRUE(gridMapOut.exists(type));
+  EXPECT_TRUE(gridMapOut.exists(layer));
 
-  dataOut = gridMapOut.get(type);
+  dataOut = gridMapOut.get(layer);
 
+  // TODO Do full matrix comparison.
   EXPECT_DOUBLE_EQ(dataIn(0,0), dataOut(0,0));
   EXPECT_DOUBLE_EQ(dataIn(1,1), dataOut(1,1));
-
 }
 
 TEST(RosbagHandling, saveLoadWithTime)
 {
-  Matrix2f dataIn, dataOut;
+  Eigen::Matrix2f dataIn, dataOut;
   dataIn << 1.0, 2.0,
             3.0, 4.0;
-  string type = "type";
-  string bagName = "bagName.bag";
-  string topicName = "topicName";
+  string layer = "layer";
+  string pathToBag = "test.bag";
+  string topic = "topic";
   grid_map::GridMap gridMapIn, gridMapOut;
-  gridMapIn.add(type, dataIn);
+  gridMapIn.add(layer, dataIn);
 
-  EXPECT_FALSE(gridMapOut.exists(type));
+  EXPECT_FALSE(gridMapOut.exists(layer));
 
   if (!ros::Time::isValid()) ros::Time::init();
-  ros::Time time = ros::Time::now();
+  // TODO Do other time than now.
+  gridMapIn.setTimestamp(ros::Time::now().toNSec());
 
-  EXPECT_TRUE(gridMapIn.saveToBag(bagName, topicName, time));
-  EXPECT_TRUE(gridMapOut.loadFromBag(bagName, topicName));
+  EXPECT_TRUE(GridMapRosConverter::saveToBag(gridMapIn, pathToBag, topic));
+  EXPECT_TRUE(GridMapRosConverter::loadFromBag(pathToBag, topic, gridMapOut));
 
-  EXPECT_TRUE(gridMapOut.exists(type));
+  EXPECT_TRUE(gridMapOut.exists(layer));
 
-  dataOut = gridMapOut.get(type);
+  dataOut = gridMapOut.get(layer);
 
+  // TODO Do full matrix comparison.
   EXPECT_DOUBLE_EQ(dataIn(0,0), dataOut(0,0));
   EXPECT_DOUBLE_EQ(dataIn(1,1), dataOut(1,1));
-
 }
