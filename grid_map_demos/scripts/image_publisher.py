d97d9f7f70454dd0f119c01d5b733e06d606fbf9
martiwer <martiwer@student.ethz.ch>
2015-04-30 16:27:01 +0200

    Changed to bgra.

--- grid_map_demos/scripts/image_publisher.py
+++ grid_map_demos/scripts/image_publisher.py
@@ -7,16 +7,16 @@ import sensor_msgs.msg
 
 #change these to fit the expected topic names
 IMAGE_MESSAGE_TOPIC = 'grid_map_image'
-IMAGE_PATH = 'test2.png'
+IMAGE_PATH = 'bgra8.png'
 
 def callback(self):
     """ Convert a image to a ROS compatible message
         (sensor_msgs.Image).
     """
-    img = cv2.imread(IMAGE_PATH, 0)
+    img = cv2.imread(IMAGE_PATH, -1)
 
     rosimage = sensor_msgs.msg.Image()
-    rosimage.encoding = 'mono16'
+    rosimage.encoding = 'bgra8'
     rosimage.width = img.shape[1]
     rosimage.height = img.shape[0]
     rosimage.step = img.strides[0]
@@ -33,7 +33,7 @@ def main_program():
     global publisher
     rospy.init_node('image_publisher')
     publisher = rospy.Publisher(IMAGE_MESSAGE_TOPIC, sensor_msgs.msg.Image, queue_size=10)
-    rospy.Timer(rospy.Duration(0.5), callback)
+    rospy.Timer(rospy.Duration(2), callback)
     rospy.spin()
 
 if __name__ == '__main__':
