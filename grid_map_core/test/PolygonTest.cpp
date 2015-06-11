d4c216387eed0cc2ec09c95cccf79382040c14a4
Peter Fankhauser <pfankhauser@ethz.ch>
2015-06-11 12:21:47 +0200

    Added a unit test for inqueality constraints generation from polygons.

--- grid_map_core/test/PolygonTest.cpp
+++ grid_map_core/test/PolygonTest.cpp
@@ -89,12 +89,29 @@ TEST(checkConvexHullCircle, createHull)
   EXPECT_FALSE(hull.isInside(Vector2d(1.0, 0.0)));
 }
 
-TEST(convertToInequalityConstraints, triangle)
+TEST(convertToInequalityConstraints, triangle1)
 {
   grid_map::Polygon polygon({Position(1.0, 1.0), Position(0.0, 0.0), Position(1.1, -1.1)});
   MatrixXd A;
   VectorXd b;
-  EXPECT_TRUE(polygon.convertToInequalityConstraints(A, b));
+  ASSERT_TRUE(polygon.convertToInequalityConstraints(A, b));
+  EXPECT_NEAR(-1.3636, A(0, 0), 1e-4);
+  EXPECT_NEAR( 1.3636, A(0, 1), 1e-4);
+  EXPECT_NEAR(-1.5000, A(1, 0), 1e-4);
+  EXPECT_NEAR(-1.5000, A(1, 1), 1e-4);
+  EXPECT_NEAR( 2.8636, A(2, 0), 1e-4);
+  EXPECT_NEAR( 0.1364, A(2, 1), 1e-4);
+  EXPECT_NEAR( 0.0000, b(0), 1e-4);
+  EXPECT_NEAR( 0.0000, b(1), 1e-4);
+  EXPECT_NEAR( 3.0000, b(2), 1e-4);
+}
+
+TEST(convertToInequalityConstraints, triangle2)
+{
+  grid_map::Polygon polygon({Position(-1.0, 0.5), Position(-1.0, -0.5), Position(1.0, -0.5)});
+  MatrixXd A;
+  VectorXd b;
+  ASSERT_TRUE(polygon.convertToInequalityConstraints(A, b));
   EXPECT_NEAR(-1.3636, A(0, 0), 1e-4);
   EXPECT_NEAR( 1.3636, A(0, 1), 1e-4);
   EXPECT_NEAR(-1.5000, A(1, 0), 1e-4);
