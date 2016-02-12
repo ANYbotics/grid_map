083eb87589c16b1fa1707970bad65eef4e73b055
Peter Fankhauser <pfankhauser@ethz.ch>
2016-02-12 10:38:53 +0100

    Added unit test for round trip conversion from occupancy grid to grid map and back to occupancy grid.

--- grid_map/test/GridMapTest.cpp
+++ grid_map/test/GridMapTest.cpp
@@ -2,7 +2,7 @@
  * GridMapTest.cpp
  *
  *  Created on: Mar 24, 2015
- *      Author: Martin Wermelinger
+ *      Author: Peter Fankhauser, Martin Wermelinger
  *	 Institute: ETH Zurich, Autonomous Systems Lab
  */
 
@@ -19,6 +19,11 @@
 // STD
 #include <string>
 #include <vector>
+#include <stdlib.h>
+#include <iterator>
+
+// ROS
+#include <nav_msgs/OccupancyGrid.h>
 
 using namespace std;
 using namespace grid_map;
@@ -104,4 +109,50 @@ TEST(OccupancyGridConversion, withMove)
 
   // Now the (0, 0) cell should be unobserved (-1).
   EXPECT_DOUBLE_EQ(-1.0, occupancyGridNew.data[0]);
+}
+
+TEST(OccupancyGridConversion, roundTrip)
+{
+  // Create occupancy grid.
+  nav_msgs::OccupancyGrid occupancyGrid;
+  occupancyGrid.header.stamp = ros::Time(5.0);
+  occupancyGrid.header.frame_id = "map";
+  occupancyGrid.info.resolution = 0.1;
+  occupancyGrid.info.width = 50;
+  occupancyGrid.info.height = 100;
+  occupancyGrid.info.origin.position.x = 3.0;
+  occupancyGrid.info.origin.position.y = 6.0;
+  occupancyGrid.info.origin.orientation.w = 1.0;
+  occupancyGrid.data.resize(occupancyGrid.info.width * occupancyGrid.info.height);
+
+  for (auto& cell : occupancyGrid.data) {
+    cell = rand() % 102 - 1; // [-1, 100]
+  }
+
+  // Convert to grid map.
+  grid_map::GridMap gridMap;
+  grid_map::GridMapRosConverter::fromOccupancyGrid(occupancyGrid, "layer", gridMap);
+
+  // Convert back to occupancy grid.
+  nav_msgs::OccupancyGrid occupancyGridResult;
+  grid_map::GridMapRosConverter::toOccupancyGrid(gridMap, "layer", -1.0, 100.0, occupancyGridResult);
+
+  // Check map info.
+  EXPECT_EQ(occupancyGrid.header.stamp, occupancyGridResult.header.stamp);
+  EXPECT_EQ(occupancyGrid.header.frame_id, occupancyGridResult.header.frame_id);
+  EXPECT_EQ(occupancyGrid.info.width, occupancyGridResult.info.width);
+  EXPECT_EQ(occupancyGrid.info.height, occupancyGridResult.info.height);
+  EXPECT_DOUBLE_EQ(occupancyGrid.info.origin.position.x, occupancyGridResult.info.origin.position.x);
+  EXPECT_DOUBLE_EQ(occupancyGrid.info.origin.position.x, occupancyGridResult.info.origin.position.x);
+  EXPECT_DOUBLE_EQ(occupancyGrid.info.origin.orientation.x, occupancyGridResult.info.origin.orientation.x);
+  EXPECT_DOUBLE_EQ(occupancyGrid.info.origin.orientation.y, occupancyGridResult.info.origin.orientation.y);
+  EXPECT_DOUBLE_EQ(occupancyGrid.info.origin.orientation.z, occupancyGridResult.info.origin.orientation.z);
+  EXPECT_DOUBLE_EQ(occupancyGrid.info.origin.orientation.w, occupancyGridResult.info.origin.orientation.w);
+
+  // Check map data.
+  for (std::vector<int8_t>::iterator iterator = occupancyGrid.data.begin();
+      iterator != occupancyGrid.data.end(); ++iterator) {
+    size_t i = std::distance(occupancyGrid.data.begin(), iterator);
+    EXPECT_EQ((int)*iterator, (int)occupancyGridResult.data[i]);
+  }
 }
