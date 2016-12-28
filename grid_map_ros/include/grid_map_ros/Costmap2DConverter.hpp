fc92b9c60df1ca03b51ea4409c474d376a73cc56
Daniel Stonier <d.stonier@gmail.com>
2016-12-28 15:14:25 +0900

    Costmap2DROS conversion at the robot pose

--- grid_map_ros/include/grid_map_ros/Costmap2DConverter.hpp
+++ grid_map_ros/include/grid_map_ros/Costmap2DConverter.hpp
@@ -14,6 +14,9 @@
 
 #include <costmap_2d/costmap_2d.h>
 #include <costmap_2d/costmap_2d_ros.h>
+#include <grid_map_core/grid_map_core.hpp>
+#include <sstream>
+#include <tf/tf.h>
 #include <vector>
 
 /*****************************************************************************
@@ -87,7 +90,7 @@ class Costmap2DConverter
 public:
   /**
    * @brief Default constructor
-   * 
+   *
    * Initiliases the cost translation table with the default policy for the template
    * map type class.
    **/
@@ -98,7 +101,7 @@ public:
 
   /**
    * @brief Initialise the cost translation table with the user specified translation table functor.
-   * 
+   *
    * @param translation_table : user provided translation table
    */
   template <typename TranslationTable>
@@ -106,18 +109,18 @@ public:
   {
     initializeCostTranslationTable(translation_table);
   }
-  
+
   virtual ~Costmap2DConverter()
   {
   }
 
-  void initializeFromCostmap2d(costmap_2d::Costmap2DROS& costmap2d, MapType& outputMap)
+  void initializeFromCostmap2D(costmap_2d::Costmap2DROS& costmap2d, MapType& outputMap)
   {
-    initializeFromCostmap2d(*(costmap2d.getCostmap()), outputMap);
+    initializeFromCostmap2D(*(costmap2d.getCostmap()), outputMap);
     outputMap.setFrameId(costmap2d.getGlobalFrameID());
   }
 
-  void initializeFromCostmap2d(const costmap_2d::Costmap2D& costmap2d, MapType& outputMap)
+  void initializeFromCostmap2D(const costmap_2d::Costmap2D& costmap2d, MapType& outputMap)
   {
     const double resolution = costmap2d.getResolution();
     Length length(costmap2d.getSizeInCellsX()*resolution, costmap2d.getSizeInCellsY()*resolution);
@@ -129,14 +132,14 @@ public:
 
   /**
    * @brief Load the costmap2d into the specified layer.
-   * 
+   *
    * @param costmap2d : from this costmap type object
    * @param layer : name of the layer to insert
    * @param outputMap : to this costmap type object
-   * 
+   *
    * @warning this does not lock the costmap2d object, you should take care to do so from outside this function.
    **/
-  bool addLayerFromCostmap2d(const costmap_2d::Costmap2D& costmap2d,
+  bool addLayerFromCostmap2D(const costmap_2d::Costmap2D& costmap2d,
                              const std::string& layer,
                              MapType& outputMap)
   {
@@ -147,12 +150,12 @@ public:
       return false;
     }
     if (!outputMap.getStartIndex().isZero()) {
-      ROS_ERROR("CostmapConverter::fromCostmap2d() does not support non-zero start indices!");
+      ROS_ERROR("CostmapConverter::fromCostmap2D() does not support non-zero start indices!");
       return false;
     }
     // Copy data.
     // Reverse iteration is required because of different conventions
-    // between Costmap2d and grid map.
+    // between Costmap2D and grid map.
     typename MapType::Matrix data(size(0), size(1));
     const size_t nCells = outputMap.getSize().prod();
     for (size_t i = 0, j = nCells - 1; i < nCells; ++i, --j) {
@@ -166,18 +169,148 @@ public:
 
   /**
    * @brief Load the costmap2d into the specified layer.
-   * 
+   *
    * @param costmap2d : from this costmap type object
    * @param layer : name of the layer to insert
    * @param outputMap : to this costmap type object
-   * 
+   *
    * @warning this does not lock the costmap2d object, you should take care to do so from outside this function.
    **/
-  bool addLayerFromCostmap2d(costmap_2d::Costmap2DROS& costmap2d,
+  bool addLayerFromCostmap2D(costmap_2d::Costmap2DROS& costmap2d,
                              const std::string& layer,
                              MapType& outputMap)
   {
-    return addLayerFromCostmap2d(*(costmap2d.getCostmap()), layer, outputMap);
+    return addLayerFromCostmap2D(*(costmap2d.getCostmap()), layer, outputMap);
+  }
+
+  bool initializeFromCostmap2DAtRobotPose(costmap_2d::Costmap2DROS& costmap2d,
+                                          const Length& geometry,
+                                          MapType& outputMap)
+  {
+    /****************************************
+    ** Properties
+    ****************************************/
+    const double resolution = costmap2d.getCostmap()->getResolution();
+
+    /****************************************
+    ** Get the Robot Pose Transform
+    ****************************************/
+    tf::Stamped<tf::Pose> tf_pose;
+    if(!costmap2d.getRobotPose(tf_pose))
+    {
+      std::ostringstream error_message;
+      error_message << "could not get robot pose, is it actually published?";
+      ROS_ERROR_STREAM("CostmapConverter::" << error_message.str());
+      return false;
+      // throw std::runtime_error(error_message.str());
+    }
+
+    /****************************************
+    ** Where is the New Costmap Origin?
+    ****************************************/
+    Position robot_position(tf_pose.getOrigin().x() , tf_pose.getOrigin().y());
+    Position ros_map_origin(costmap2d.getCostmap()->getOriginX(), costmap2d.getCostmap()->getOriginY());
+    Position new_cost_map_origin;
+
+    // Note:
+    //   You cannot directly use the robot pose as the new 'costmap centre'
+    //   since the underlying grid is not necessarily exactly aligned with
+    //   that (two cases to consider, rolling window and globally fixed).
+    //
+    // Relevant diagrams:
+    //  - https://github.com/ethz-asl/grid_map
+
+    // float versions of the cell co-ordinates, use static_cast<int> to get the indices
+    Position robot_cell_position = (robot_position - ros_map_origin)/resolution;
+
+    // if there is an odd number of cells
+    //   centre of the new grid map in the centre of the current cell
+    // if there is an even number of cells
+    //   centre of the new grid map at the closest vertex between cells
+    // of the current cell
+    int number_of_cells_x = geometry.x()/resolution;
+    int number_of_cells_y = geometry.y()/resolution;
+    if ( number_of_cells_x % 2 ) { // odd
+      new_cost_map_origin(0) = std::floor(robot_cell_position.x())*resolution + resolution/2.0 + ros_map_origin.x();
+    } else {
+      new_cost_map_origin(0) = std::round(robot_cell_position.x())*resolution + ros_map_origin.x();
+    }
+    if ( number_of_cells_y % 2 ) { // odd
+      new_cost_map_origin(1) = std::floor(robot_cell_position.y())*resolution + resolution/2.0 + ros_map_origin.y();
+    } else {
+      new_cost_map_origin(1) = std::round(robot_cell_position.y())*resolution + ros_map_origin.y();
+    }
+
+    /****************************************
+    ** Asserts
+    ****************************************/
+    // TODO check the robot pose is in the window
+    // TODO check the geometry fits within the costmap2d window
+
+    /****************************************
+    ** Initialise the Output Map
+    ****************************************/
+    outputMap.setFrameId(costmap2d.getGlobalFrameID());
+    outputMap.setTimestamp(ros::Time::now().toNSec());
+    outputMap.setGeometry(geometry, resolution, new_cost_map_origin);
+    return true;
+  }
+
+  bool addLayerFromCostmap2DAtRobotPose(costmap_2d::Costmap2DROS& costmap2d,
+                                        const std::string& layer,
+                                        MapType& outputMap)
+  {
+    /****************************************
+    ** Asserts
+    ****************************************/
+    if ( outputMap.getResolution() != costmap2d.getCostmap()->getResolution()) {
+      std::ostringstream error_message;
+      error_message << "Costmap2D and output map have different resolutions!";
+      ROS_ERROR_STREAM("CostmapConverter::" << error_message.str());
+      return false;
+    }
+    // 1) would be nice to check the output map centre has been initialised where it should be
+    //      i.e. the robot pose didn't move since or the initializeFrom wasn't called
+    //    but this would mean listening to tf's again and anyway, it gets shifted to make sure
+    //    the costmaps align, so these wouldn't be exactly the same anyway
+    // 2) check the geometry fits inside the costmap2d subwindow is done below
+
+    /****************************************
+    ** Properties
+    ****************************************/
+    const double resolution = costmap2d.getCostmap()->getResolution();
+//    const Length geometry = outputMap.getLength();
+//    // Note: don't use getSizeInMeters() here - it doesn't give the actual grid size...for
+//    // some reason they designed it so that it cuts it short at the centre of the last cell
+//    //            e.g. 10 cells, resolution 1.0 -> getSizeInMeters() == 9.5
+//    double original_size_x = costmap2d.getCostmap()->getSizeInCellsX() * resolution;
+//    double original_size_y = costmap2d.getCostmap()->getSizeInCellsY() * resolution;
+//    // Note: don't use getSizeInMeters() here - it doesn't give the actual grid size...for
+//    // some reason they designed it so that it cuts it short at the centre of the last cell
+//    //            e.g. 10 cells, resolution 1.0 -> getSizeInMeters() == 9.5
+    const Length geometry = outputMap.getLength();
+
+    /****************************************
+    ** Copy Data
+    ****************************************/
+    bool is_valid_window = false;
+    costmap_2d::Costmap2D costmap_subwindow;
+    is_valid_window = costmap_subwindow.copyCostmapWindow(
+                            *(costmap2d.getCostmap()),
+                            outputMap.getPosition().x() - geometry.x() / 2.0, // subwindow_bottom_left_x
+                            outputMap.getPosition().y() - geometry.y() / 2.0, // subwindow_bottom_left_y
+                            geometry.x(),
+                            geometry.y());
+    if ( !is_valid_window ) {
+      // handle differently - e.g. copy the internal part only and lethal elsewhere, but other parts would have to handle being outside too
+      std::ostringstream error_message;
+      error_message << "subwindow landed outside the costmap, aborting";
+      ROS_ERROR_STREAM("CostmapConverter::" << error_message.str());
+      return false;
+      throw std::out_of_range(error_message.str());
+    }
+    addLayerFromCostmap2D(costmap_subwindow, layer, outputMap);
+    return true;
   }
 
 private:
