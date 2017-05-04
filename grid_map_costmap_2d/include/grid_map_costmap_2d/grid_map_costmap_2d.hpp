787747e568edc2f05c1a1d7841e0682424087507
Peter Fankhauser <pfankhauser@ethz.ch>
2017-05-04 10:40:39 +0200

    Move costmap_2d conversion into separate package.

---
+++ grid_map_costmap_2d/include/grid_map_costmap_2d/grid_map_costmap_2d.hpp
@@ -0,0 +1,12 @@
+/*
+ * grid_map.hpp
+ *
+ *  Created on: Jul 14, 2014
+ *      Author: Péter Fankhauser
+ *	 Institute: ETH Zurich, Autonomous Systems Lab
+ */
+
+#pragma once
+
+#include <grid_map_core/grid_map_core.hpp>
+#include <grid_map_costmap_2d/Costmap2DConverter.hpp>
