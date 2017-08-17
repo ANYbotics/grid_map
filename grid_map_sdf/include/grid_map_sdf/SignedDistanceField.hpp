aac8f5a1d30fd2f6cd8aa7ce8aba935dbd41be79
Takahiro Miki <takahiro.miki1992@gmail.com>
2017-08-17 17:24:24 +0200

    Fix compilation error

--- grid_map_sdf/include/grid_map_sdf/SignedDistanceField.hpp
+++ grid_map_sdf/include/grid_map_sdf/SignedDistanceField.hpp
@@ -14,9 +14,7 @@
 #include <grid_map_core/GridMap.hpp>
 
 //pcl
-#include <pcl_conversions/pcl_conversions.h>
 #include <pcl/point_types.h>
-#include <pcl/PCLPointCloud2.h>
 #include <pcl/conversions.h>
 
 using namespace grid_map;
