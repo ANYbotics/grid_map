95671b551b018f2baf1101341d4dc9876ccd3265
Perry Franklin <fperry@student.ethz.ch>
2017-10-26 16:20:42 +0200

    This looks like a lot, but it actually is mostly a formatting change. The only functional change is on line 47. The rest is just a tab change.

--- grid_map_core/src/iterators/NearestValidIterator.cpp
+++ grid_map_core/src/iterators/NearestValidIterator.cpp
@@ -42,43 +42,45 @@ NearestValidIterator& NearestValidIterator::operator ++(){
     valid_index_queue_.pop();
   }
 
-    while(!isPastEnd()){
-      // The closest distance possible in this ring of the SpiralGridIterator to the starting point
-      // is from the edge of the starting index to the inner edge of the ring being searched.
-      // This is an lower bound for a starting position anywhere in the starting square.
-      const double current_closest_spiral_distance = grid_map_.getResolution()*(spiral_grid_iterator_->getCurrentRadius()-1);
+  // This while loop uses the SpiralGridIterator to search for the next nearest point.
+  // So if the spiral_grid_iterator is past the end, we've already search all valid points.
+  while(!spiral_grid_iterator_->isPastEnd()){
+    // The closest distance possible in this ring of the SpiralGridIterator to the starting point
+    // is from the edge of the starting index to the inner edge of the ring being searched.
+    // This is an lower bound for a starting position anywhere in the starting square.
+    const double current_closest_spiral_distance = grid_map_.getResolution()*(spiral_grid_iterator_->getCurrentRadius()-1);
 
-      double current_closest_valid_distance;
-      if (!valid_index_queue_.empty()){
-        current_closest_valid_distance= valid_index_queue_.top().distance;
-      } else {
-        current_closest_valid_distance = std::numeric_limits<double>::max();
-      }
-
-      // If the closest possible point in this spiral is further away that the current best, we stop searching.
-      if (current_closest_spiral_distance > current_closest_valid_distance) {
-        break;
-      }
-
-
-      const Index& current_check_index = **spiral_grid_iterator_;
-
-      if (index_checker_->check(current_check_index)){
-        // We've found a valid cell.
-        DistanceIndexPair di_pair;
-        di_pair.index = current_check_index;
-
-        Position index_position;
-        grid_map_.getPosition(current_check_index, index_position);
-
-        di_pair.distance = (index_position - starting_position_).norm();
-
-        valid_index_queue_.push(di_pair);
-      }
-
-      ++(*spiral_grid_iterator_);
-
+    double current_closest_valid_distance;
+    if (!valid_index_queue_.empty()){
+      current_closest_valid_distance= valid_index_queue_.top().distance;
+    } else {
+      current_closest_valid_distance = std::numeric_limits<double>::max();
     }
+
+    // If the closest possible point in this spiral is further away that the current best, we stop searching.
+    if (current_closest_spiral_distance > current_closest_valid_distance) {
+      break;
+    }
+
+
+    const Index& current_check_index = **spiral_grid_iterator_;
+
+    if (index_checker_->check(current_check_index)){
+      // We've found a valid cell.
+      DistanceIndexPair di_pair;
+      di_pair.index = current_check_index;
+
+      Position index_position;
+      grid_map_.getPosition(current_check_index, index_position);
+
+      di_pair.distance = (index_position - starting_position_).norm();
+
+      valid_index_queue_.push(di_pair);
+    }
+
+    ++(*spiral_grid_iterator_);
+
+  }
 
   return *this;
 }
