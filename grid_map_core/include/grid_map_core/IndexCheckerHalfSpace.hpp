cde13ca11ce88b0a0222b24a46c08946469f4b20
Perry Franklin <fperry@student.ethz.ch>
2017-11-01 16:16:30 +0100

    Adds a index checker that checks if a cell intersects with a halfspace

---
+++ grid_map_core/include/grid_map_core/IndexCheckerHalfSpace.hpp
@@ -0,0 +1,67 @@
+/*
+ * IndexCheckerHalfSpace.hpp
+ *
+ *  Created on: Nov 1, 2017
+ *      Author: Perry Franklin
+ */
+
+
+#pragma once
+
+#include "grid_map_core/IndexChecker.hpp"
+
+namespace grid_map {
+
+/*!
+ * The IndexCheckerHalfSpace class implements the IndexChecker.
+ * It checks an index lies within a halfspace (ie on one side
+ * of the plane). Computes in the form of:
+ *    A*x < c
+ * where A is the normal, c is an offset, and x is a point
+ * in the grid cell.
+ * Note: if any of the grid cell is within the halfspace, it
+ * returns true.
+ */
+class IndexCheckerHalfSpace : public IndexChecker {
+
+public:
+
+  /*!
+   * Constructor.
+   * @param gridMap the GridMap to query.
+   * @param normal the normal of the splitting plane (points away from the valid spaces)
+   * @param a point on the splitting plane
+   */
+  IndexCheckerHalfSpace(const GridMap& map, const Vector& normal, double offset);
+
+  /*!
+   * Constructor.
+   * @param gridMap the GridMap to query.
+   * @param normal the normal of the splitting plane (points away from the valid spaces)
+   * @param a point on the splitting plane
+   */
+  IndexCheckerHalfSpace(const GridMap& map, const Vector& normal, const Position& plane_point);
+
+  /*!
+   * Checks if GridMap at index is NaN.
+   * @param index the index to check.
+   */
+  bool check(const Index& index) const override;
+
+  IndexChecker* clone() const override;
+
+private:
+
+  /*!
+   * Copy Constructor
+   * @param other the other IndexCheckerHalfSpace to copy.
+   */
+  IndexCheckerHalfSpace(const IndexCheckerHalfSpace& other);
+
+  Vector normal_;
+  double offset_;
+  Position cell_corner_offset_;
+
+};
+
+}  // namespace grid_map
---
+++ grid_map_core/src/IndexCheckerHalfSpace.cpp
@@ -0,0 +1,94 @@
+/*
+ * IndexCheckerHalfSpace.cpp
+ *
+ *  Created on: Nov 1, 2017
+ *      Author: Perry Franklin
+ */
+
+#include "grid_map_core/IndexCheckerHalfSpace.hpp"
+
+namespace grid_map {
+
+IndexCheckerHalfSpace::IndexCheckerHalfSpace(const GridMap& map, const Vector& normal, double offset):
+    IndexChecker(map),
+    normal_(normal.normalized()),
+    offset_(offset){
+
+  // We only need to check one corner of the cell to see if any of the cell lies in the halfspace.
+  // Specifically, the opposite corner from
+  if (normal.x() >= 0){
+    if (normal.y() >= 0){
+      cell_corner_offset_ = map_.getResolution()*Position(-1.0,-1.0);
+    }
+    else
+    {
+      cell_corner_offset_ = map_.getResolution()*Position(-1.0,1.0);
+    }
+  }
+  else
+  {
+    if (normal.y() >= 0){
+      cell_corner_offset_ = map_.getResolution()*Position(1.0,-1.0);
+    }
+    else
+    {
+      cell_corner_offset_ = map_.getResolution()*Position(1.0,1.0);
+    }
+  }
+}
+
+IndexCheckerHalfSpace::IndexCheckerHalfSpace(const GridMap& map, const Vector& normal, const Position& plane_point):
+    IndexChecker(map),
+    normal_(normal.normalized()){
+
+  offset_ = normal_.dot(plane_point);
+
+  if (normal.x() >= 0){
+    if (normal.y() >= 0){
+      cell_corner_offset_ = map_.getResolution()*Position(-1.0,-1.0);
+    }
+    else
+    {
+      cell_corner_offset_ = map_.getResolution()*Position(-1.0,1.0);
+    }
+  }
+  else
+  {
+    if (normal.y() >= 0){
+      cell_corner_offset_ = map_.getResolution()*Position(1.0,-1.0);
+    }
+    else
+    {
+      cell_corner_offset_ = map_.getResolution()*Position(1.0,1.0);
+    }
+  }
+}
+
+IndexCheckerHalfSpace::IndexCheckerHalfSpace(const IndexCheckerHalfSpace& other):
+  IndexChecker(map_),
+  normal_(other.normal_),
+  offset_(other.offset_),
+  cell_corner_offset_(other.cell_corner_offset_){
+
+}
+
+
+bool IndexCheckerHalfSpace::check(const Index& index) const{
+  Position center_of_index;
+  if (!map_.getPosition(index, center_of_index)){
+    return false;
+  }
+
+  return normal_.dot(center_of_index+cell_corner_offset_) < offset_;
+}
+
+IndexChecker* IndexCheckerHalfSpace::clone() const{
+  return (new IndexCheckerHalfSpace(*this));
+}
+
+}  // namespace grid_map
+
+
+
+
+
