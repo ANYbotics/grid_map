37e6e8f1d5072547ccde9687703151caac3b2ad7
Florian Beck <florian.beck@tuwien.ac.at>
2018-07-30 09:31:33 +0000

    melodic tf fix

--- grid_map_rviz_plugin/include/grid_map_rviz_plugin/modified/message_filter_display.h
+++ grid_map_rviz_plugin/include/grid_map_rviz_plugin/modified/message_filter_display.h
@@ -23,9 +23,9 @@ class MessageFilterDisplayMod : public rviz::MessageFilterDisplay<MessageType>
 
   void onInitialize()
   {
-    MFDClass::tf_filter_ = new tf::MessageFilter<MessageType>(*MFDClass::context_->getTFClient(),
-                                                              MFDClass::fixed_frame_.toStdString(),
-                                                              10, MFDClass::update_nh_);
+    MFDClass::tf_filter_ = new tf2_ros::MessageFilter<MessageType>(*MFDClass::context_->getTF2BufferPtr(),
+                                                                   MFDClass::fixed_frame_.toStdString(),
+                                                                   10, MFDClass::update_nh_);
 
     MFDClass::tf_filter_->connectInput(MFDClass::sub_);
     MFDClass::tf_filter_->registerCallback(
