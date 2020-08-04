94f44968d58c3c2af7321110cf31823649facda1
Marwan Taher <marokhaled99@gmail.com>
2020-08-04 01:08:51 +0100

    message_traits.hpp ported

--- grid_map_ros/include/grid_map_ros/message_traits.hpp
+++ grid_map_ros/include/grid_map_ros/message_traits.hpp
@@ -1,73 +1,117 @@
 #ifndef GRID_MAP_ROS__MESSAGE_TRAITS_HPP_
 #define GRID_MAP_ROS__MESSAGE_TRAITS_HPP_
 
-#include <grid_map_msgs/GridMap.h>
+#include <std_msgs/msg/header.hpp>
+#include <rclcpp/rclcpp.hpp>
 
 #include <string>
+#include <type_traits>
+#include <memory>
 
-namespace ros
+namespace grid_map
 {
 namespace message_traits
 {
 
-template<>
-struct HasHeader<grid_map_msgs::GridMap>: public TrueType {};
+template<typename M, typename = void>
+struct HasHeader : public std::false_type {};
 
-template<>
-struct Header<grid_map_msgs::GridMap,
-  typename boost::enable_if<HasHeader<grid_map_msgs::GridMap>>::type>
+template<typename M>
+struct HasHeader<M, decltype((void) M::header)>: std::true_type {};
+
+template<typename M, typename Enable = void>
+struct Header
 {
-  static std_msgs::Header * pointer(grid_map_msgs::GridMap & m)
+  static std_msgs::msg::Header * pointer(M & m)
   {
-    return &m.info.header;
+    (void)m;
+    return nullptr;
   }
-
-  static std_msgs::Header const * pointer(const grid_map_msgs::GridMap & m)
+  static std_msgs::msg::Header const * pointer(const M & m)
   {
-    return &m.info.header;
+    (void)m;
+    return nullptr;
   }
 };
 
-template<>
-struct FrameId<grid_map_msgs::GridMap,
-  typename boost::enable_if<HasHeader<grid_map_msgs::GridMap>>::type>
+template<typename M>
+struct Header<M, typename std::enable_if<HasHeader<M>::value>::type>
 {
-  static std::string * pointer(grid_map_msgs::GridMap & m)
+  static std_msgs::msg::Header * pointer(M & m)
   {
-    return &m.info.header.frame_id;
+    return &m.header;
   }
 
-  static std::string const * pointer(const grid_map_msgs::GridMap & m)
+  static std_msgs::msg::Header const * pointer(const M & m)
   {
-    return &m.info.header.frame_id;
-  }
-
-  static std::string value(const grid_map_msgs::GridMap & m)
-  {
-    return m.info.header.frame_id;
+    return &m.header;
   }
 };
 
-template<>
-struct TimeStamp<grid_map_msgs::GridMap,
-  typename boost::enable_if<HasHeader<grid_map_msgs::GridMap>>::type>
+template<typename M, typename Enable = void>
+struct FrameId
 {
-  static rclcpp::Time * pointer(grid_map_msgs::GridMap & m)
+  static std::string * pointer(M & m)
   {
-    return &m.info.header.stamp;
+    (void)m;
+    return nullptr;
+  }
+  static std::string const * pointer(const M & m)
+  {
+    (void)m;
+    return nullptr;
+  }
+};
+
+template<typename M>
+struct FrameId<M, typename std::enable_if<HasHeader<M>::value>::type>
+{
+  static std::string * pointer(M & m)
+  {
+    return &m.header.frame_id;
+  }
+  static std::string const * pointer(const M & m)
+  {
+    return &m.header.frame_id;
+  }
+  static std::string value(const M & m)
+  {
+    return m.header.frame_id;
+  }
+};
+
+template<typename M, typename Enable = void>
+struct TimeStamp
+{
+  static std::unique_ptr<rclcpp::Time> pointer(const M & m)
+  {
+    (void)m;
+    return nullptr;
   }
 
-  static rclcpp::Time const * pointer(const grid_map_msgs::GridMap & m)
+  static rclcpp::Time value(const M & m)
   {
-    return &m.info.header.stamp;
+    (void)m;
+    return rclcpp::Time();
+  }
+};
+
+template<typename M>
+struct TimeStamp<M, typename std::enable_if<HasHeader<M>::value>::type>
+{
+  static std::unique_ptr<rclcpp::Time> pointer(const M & m)
+  {
+    auto stamp = m.header.stamp;
+    return std::make_unique<rclcpp::Time>(rclcpp::Time(stamp.sec, stamp.nanosec));
   }
 
-  static rclcpp::Time value(const grid_map_msgs::GridMap & m)
+  static rclcpp::Time value(const M & m)
   {
-    return m.info.header.stamp;
+    auto stamp = m.header.stamp;
+    return rclcpp::Time(stamp.sec, stamp.nanosec);
   }
 };
 
 }  // namespace message_traits
-}  // namespace ros
+}  // namespace grid_map
 #endif  // GRID_MAP_ROS__MESSAGE_TRAITS_HPP_
