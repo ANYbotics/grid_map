84eefca00edf4e15a872d247fb409f877a54908e
Marwan Taher <marokhaled99@gmail.com>
2020-09-13 04:08:16 +0100

    opencv_demo ported
    Signed-off-by: Marwan Taher <marokhaled99@gmail.com>

--- grid_map_demos/src/opencv_demo_node.cpp
+++ grid_map_demos/src/opencv_demo_node.cpp
@@ -1,22 +1,26 @@
-#include <ros/ros.h>
+#include <rclcpp/rclcpp.hpp>
 #include <grid_map_ros/grid_map_ros.hpp>
 #include <grid_map_cv/grid_map_cv.hpp>
 #include <cv_bridge/cv_bridge.h>
 #include <opencv2/highgui/highgui.hpp>
+#include <utility>
 
 int main(int argc, char ** argv)
 {
   // Initialize node and publisher.
-  init(argc, argv, "opencv_demo");
-  NodeHandle nodeHandle("~");
-  Publisher publisher = nodeHandle.advertise<grid_map_msgs::GridMap>("grid_map", 1, true);
+  rclcpp::init(argc, argv);
+  rclcpp::Node node("opencv_demo");
+  auto publisher = node.create_publisher<grid_map_msgs::msg::GridMap>(
+    "grid_map", rclcpp::QoS(
+      1).transient_local());
   const bool useTransparency = false;
 
   // Create grid map.
-  GridMap map({"original", "elevation"});
+  grid_map::GridMap map({"original", "elevation"});
   map.setFrameId("map");
-  map.setGeometry(Length(1.2, 2.0), 0.01);
-  ROS_INFO(
+  map.setGeometry(grid_map::Length(1.2, 2.0), 0.01);
+  RCLCPP_INFO(
+    node.get_logger(),
     "Created map with size %f x %f m (%i x %i cells).",
     map.getLength().x(), map.getLength().y(),
     map.getSize()(0), map.getSize()(1));
@@ -25,17 +29,17 @@ int main(int argc, char ** argv)
   if (!useTransparency) {map["original"].setZero();}
   grid_map::Polygon polygon;
   polygon.setFrameId(map.getFrameId());
-  polygon.addVertex(Position(0.480, 0.000));
-  polygon.addVertex(Position(0.164, 0.155));
-  polygon.addVertex(Position(0.116, 0.500));
-  polygon.addVertex(Position(-0.133, 0.250));
-  polygon.addVertex(Position(-0.480, 0.399));
-  polygon.addVertex(Position(-0.316, 0.000));
-  polygon.addVertex(Position(-0.480, -0.399));
-  polygon.addVertex(Position(-0.133, -0.250));
-  polygon.addVertex(Position(0.116, -0.500));
-  polygon.addVertex(Position(0.164, -0.155));
-  polygon.addVertex(Position(0.480, 0.000));
+  polygon.addVertex(grid_map::Position(0.480, 0.000));
+  polygon.addVertex(grid_map::Position(0.164, 0.155));
+  polygon.addVertex(grid_map::Position(0.116, 0.500));
+  polygon.addVertex(grid_map::Position(-0.133, 0.250));
+  polygon.addVertex(grid_map::Position(-0.480, 0.399));
+  polygon.addVertex(grid_map::Position(-0.316, 0.000));
+  polygon.addVertex(grid_map::Position(-0.480, -0.399));
+  polygon.addVertex(grid_map::Position(-0.133, -0.250));
+  polygon.addVertex(grid_map::Position(0.116, -0.500));
+  polygon.addVertex(grid_map::Position(0.164, -0.155));
+  polygon.addVertex(grid_map::Position(0.480, 0.000));
 
   for (grid_map::PolygonIterator iterator(map, polygon); !iterator.isPastEnd(); ++iterator) {
     map.at("original", *iterator) = 0.3;
@@ -46,11 +50,11 @@ int main(int argc, char ** argv)
   if (useTransparency) {
     // Note: The template parameters have to be set based on your encoding
     // of the image. For 8-bit images use `unsigned char`.
-    GridMapCvConverter::toImage<unsigned int16_t, 4>(
+    grid_map::GridMapCvConverter::toImage<uint16_t, 4>(
       map, "original", CV_16UC4, 0.0, 0.3,
       originalImage);
   } else {
-    GridMapCvConverter::toImage<unsigned int16_t, 1>(
+    grid_map::GridMapCvConverter::toImage<uint16_t, 1>(
       map, "original", CV_16UC1, 0.0, 0.3,
       originalImage);
   }
@@ -59,12 +63,12 @@ int main(int argc, char ** argv)
   cv::namedWindow("OpenCV Demo");
 
   // Work with copy of image in a loop.
-  while (nodeHandle.ok()) {
+  while (rclcpp::ok()) {
     // Initialize.
-    ros::Time time = ros::Time::now();
-    map.setTimestamp(time.toNSec());
+    rclcpp::Time time = node.now();
+    map.setTimestamp(time.nanoseconds());
     cv::Mat modifiedImage;
-    int blurRadius = 200 - abs(static_cast<int>(200.0 * sin(time.toSec())));
+    int blurRadius = 200 - abs(static_cast<int>(200.0 * sin(time.seconds())));
     blurRadius = blurRadius - (blurRadius % 2) + 1;
 
     // Apply Gaussian blur.
@@ -76,20 +80,20 @@ int main(int argc, char ** argv)
 
     // Convert resulting image to a grid map.
     if (useTransparency) {
-      GridMapCvConverter::addLayerFromImage<unsigned int16_t, 4>(
+      grid_map::GridMapCvConverter::addLayerFromImage<uint16_t, 4>(
         modifiedImage, "elevation", map, 0.0,
         0.3, 0.3);
     } else {
-      GridMapCvConverter::addLayerFromImage<unsigned int16_t, 1>(
+      grid_map::GridMapCvConverter::addLayerFromImage<uint16_t, 1>(
         modifiedImage, "elevation", map, 0.0,
         0.3);
     }
 
     // Publish grid map.
-    grid_map_msgs::GridMap message;
-    GridMapRosConverter::toMessage(map, message);
-    publisher.publish(message);
-    ROS_INFO_STREAM("Published image and grid map with blur radius " << blurRadius << ".");
+    auto message = grid_map::GridMapRosConverter::toMessage(map);
+    publisher->publish(std::move(message));
+    RCLCPP_INFO_STREAM(
+      node.get_logger(), "Published image and grid map with blur radius " << blurRadius << ".");
   }
 
   return 0;
