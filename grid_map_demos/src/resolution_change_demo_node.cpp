03fbae4b48e7d06c9d1398d0e03154c6ff222121
Marwan Taher <marokhaled99@gmail.com>
2020-09-13 19:46:42 +0100

    resolution_change_demo ported
    Signed-off-by: Marwan Taher <marokhaled99@gmail.com>

--- grid_map_demos/src/resolution_change_demo_node.cpp
+++ grid_map_demos/src/resolution_change_demo_node.cpp
@@ -1,19 +1,23 @@
-#include <ros/ros.h>
+#include <rclcpp/rclcpp.hpp>
 #include <grid_map_ros/grid_map_ros.hpp>
 #include <grid_map_cv/grid_map_cv.hpp>
+#include <utility>
 
 int main(int argc, char ** argv)
 {
   // Initialize node and publisher.
-  init(argc, argv, "resolution_change_demo");
-  NodeHandle nodeHandle("~");
-  Publisher publisher = nodeHandle.advertise<grid_map_msgs::GridMap>("grid_map", 1, true);
+  rclcpp::init(argc, argv);
+  rclcpp::Node node("resolution_change_demo");
+  auto publisher = node.create_publisher<grid_map_msgs::msg::GridMap>(
+    "grid_map", rclcpp::QoS(
+      1).transient_local());
 
   // Create grid map.
-  GridMap map({"elevation"});
+  grid_map::GridMap map({"elevation"});
   map.setFrameId("map");
-  map.setGeometry(Length(1.2, 2.0), 0.03);
-  ROS_INFO(
+  map.setGeometry(grid_map::Length(1.2, 2.0), 0.03);
+  RCLCPP_INFO(
+    node.get_logger(),
     "Created map with size %f x %f m (%i x %i cells).",
     map.getLength().x(), map.getLength().y(),
     map.getSize()(0), map.getSize()(1));
@@ -22,39 +26,39 @@ int main(int argc, char ** argv)
   map["elevation"].setZero();  // Un/comment this line to try with and without transparency.
   grid_map::Polygon polygon;
   polygon.setFrameId(map.getFrameId());
-  polygon.addVertex(Position(0.480, 0.000));
-  polygon.addVertex(Position(0.164, 0.155));
-  polygon.addVertex(Position(0.116, 0.500));
-  polygon.addVertex(Position(-0.133, 0.250));
-  polygon.addVertex(Position(-0.480, 0.399));
-  polygon.addVertex(Position(-0.316, 0.000));
-  polygon.addVertex(Position(-0.480, -0.399));
-  polygon.addVertex(Position(-0.133, -0.250));
-  polygon.addVertex(Position(0.116, -0.500));
-  polygon.addVertex(Position(0.164, -0.155));
-  polygon.addVertex(Position(0.480, 0.000));
+  polygon.addVertex(grid_map::Position(0.480, 0.000));
+  polygon.addVertex(grid_map::Position(0.164, 0.155));
+  polygon.addVertex(grid_map::Position(0.116, 0.500));
+  polygon.addVertex(grid_map::Position(-0.133, 0.250));
+  polygon.addVertex(grid_map::Position(-0.480, 0.399));
+  polygon.addVertex(grid_map::Position(-0.316, 0.000));
+  polygon.addVertex(grid_map::Position(-0.480, -0.399));
+  polygon.addVertex(grid_map::Position(-0.133, -0.250));
+  polygon.addVertex(grid_map::Position(0.116, -0.500));
+  polygon.addVertex(grid_map::Position(0.164, -0.155));
+  polygon.addVertex(grid_map::Position(0.480, 0.000));
 
   for (grid_map::PolygonIterator iterator(map, polygon); !iterator.isPastEnd(); ++iterator) {
     map.at("elevation", *iterator) = 0.3;
   }
 
-  Rate rate(10.0);
+  rclcpp::Rate rate(10.0);
 
   // Work in a loop.
-  while (nodeHandle.ok()) {
+  while (rclcpp::ok()) {
     // Initialize.
-    ros::Time time = ros::Time::now();
-    const double resolution = 0.05 + 0.04 * sin(time.toSec());
+    rclcpp::Time time = node.now();
+    const double resolution = 0.05 + 0.04 * sin(time.seconds());
 
     // Change resoltion of grid map.
-    GridMap modifiedMap;
-    GridMapCvProcessing::changeResolution(map, modifiedMap, resolution);
+    grid_map::GridMap modifiedMap;
+    grid_map::GridMapCvProcessing::changeResolution(map, modifiedMap, resolution);
 
     // Publish grid map.
-    grid_map_msgs::GridMap message;
-    GridMapRosConverter::toMessage(modifiedMap, message);
-    publisher.publish(message);
-    ROS_INFO_STREAM("Published grid map with " << resolution << " m/cell resolution.");
+    auto message = grid_map::GridMapRosConverter::toMessage(modifiedMap);
+    publisher->publish(std::move(message));
+    RCLCPP_INFO_STREAM(
+      node.get_logger(), "Published grid map with " << resolution << " m/cell resolution.");
 
     rate.sleep();
   }
