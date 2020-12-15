61972c8732aef46d3cf3a61ad435f9b66a6e65e6
Magnus Gärtner <mgaertner@anybotics.com>
2020-12-15 14:34:18 +0000

    Merge branch 'fix/visibility_cleanup' into 'master'
    [elevation_mapping]  Fix ray-tracing in elevation map on the edge of sensor window
    
    Closes #UNKNOWN
    
    GitOrigin-RevId: 6919a0b4e384d4b90f0fbf65e537869b8d2083f7

--- grid_map_core/src/GridMap.cpp
+++ grid_map_core/src/GridMap.cpp
@@ -751,23 +751,13 @@ void GridMap::clearAll() {
 }
 
 void GridMap::clearRows(unsigned int index, unsigned int nRows) {
-  std::vector<std::string> layersToClear;
-  if (basicLayers_.size() > 0)
-    layersToClear = basicLayers_;
-  else
-    layersToClear = layers_;
-  for (auto& layer : layersToClear) {
+  for (auto& layer : layers_) {
     data_.at(layer).block(index, 0, nRows, getSize()(1)).setConstant(NAN);
   }
 }
 
 void GridMap::clearCols(unsigned int index, unsigned int nCols) {
-  std::vector<std::string> layersToClear;
-  if (basicLayers_.size() > 0)
-    layersToClear = basicLayers_;
-  else
-    layersToClear = layers_;
-  for (auto& layer : layersToClear) {
+  for (auto& layer : layers_) {
     data_.at(layer).block(0, index, getSize()(0), nCols).setConstant(NAN);
   }
 }
