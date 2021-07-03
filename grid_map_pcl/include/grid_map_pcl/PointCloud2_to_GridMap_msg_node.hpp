15d26b3d86a6e1c0f21b9ddc2c5b07f8e9496384
Maximilian Stölzle <maximilian@stoelzle.ch>
2021-07-03 15:00:04 +0200

    Add nodeHandler

--- grid_map_pcl/include/grid_map_pcl/PointCloud2_to_GridMap_msg_node.hpp
+++ grid_map_pcl/include/grid_map_pcl/PointCloud2_to_GridMap_msg_node.hpp
@@ -12,14 +12,28 @@
 //namespace grid_map_pcl {
 class PointCloud2ToGridMapMsgNode
 {
+    public:
+        /*!
+        * Constructor.
+        * @param nodeHandle the ROS node handle.
+        */
+        PointCloud2ToGridMapMsgNode(ros::NodeHandle& nodeHandle);
+        ~PointCloud2ToGridMapMsgNode();
+
+        /*!
+        * Callback function for the point cloud 2.
+        * @param message the point cloud2 message to be converted to a grid map msg
+        */
+        void callback(const sensor_msgs::PointCloud2 & msg);
+
     private:
         std::string gridMapTopic_;
         std::string pointCloudTopic_;
 
+        //! ROS nodehandle.
+        ros::NodeHandle nodeHandle_;
+
         ros::Subscriber sub_;
         ros::Publisher pub_;
-    public:
-        PointCloud2ToGridMapMsgNode(ros::NodeHandle& nodeHandle);
-        ~PointCloud2ToGridMapMsgNode();
 };
 //}
--- grid_map_pcl/src/PointCloud2_to_GridMap_msg_node.cpp
+++ grid_map_pcl/src/PointCloud2_to_GridMap_msg_node.cpp
@@ -20,16 +20,19 @@ namespace gm = ::grid_map::grid_map_pcl;
 PointCloud2ToGridMapMsgNode::PointCloud2ToGridMapMsgNode(ros::NodeHandle& nodeHandle)
 {
   ROS_INFO("PointCloud2ToGridMapMsgNode started.");
+  nodeHandle_ = nodeHandle;
+
   // activityCheckTimer_ = nodeHandle_.createTimer(activityCheckDuration_,
   //                                               &GridMapVisualization::updateSubscriptionCallback,
   //                                               this);
+
   // Publisher
-  // nh.param<std::string>("grid_map_topic", gridMapTopic_, "grid_map");
-  // pub_ = nh.advertise<grid_map_msgs::GridMap>(gridMapTopic_, 1, true);
+  nodeHandle_.param<std::string>("grid_map_topic", gridMapTopic_, "grid_map");
+  pub_ = nodeHandle_.advertise<grid_map_msgs::GridMap>(gridMapTopic_, 1, true);
 
   // // Subscriber
-  // nh.param<std::string>("point_cloud_topic", pointCloudTopic_, "point_cloud");
-  // sub_ = nh.subscribe(pointCloudTopic_, 1, callback);
+  // nodeHandle_.param<std::string>("point_cloud_topic", pointCloudTopic_, "point_cloud");
+  // sub_ = nodeHandle_.subscribe(pointCloudTopic_, 1, callback);
 
 }
 
@@ -38,30 +41,30 @@ PointCloud2ToGridMapMsgNode::~PointCloud2ToGridMapMsgNode()
   ROS_INFO("PointCloud2ToGridMapMsgNode deconstructed.");
 }
 
-void callback(const sensor_msgs::PointCloud2::ConstPtr& msg) {
-  // gridMapPclLoader.loadCloudFromMessage(pathToCloud);
+// void callback(const sensor_msgs::PointCloud2::ConstPtr& msg) {
+//   // gridMapPclLoader.loadCloudFromMessage(pathToCloud);
 
-  // grid_map::GridMapPclLoader gridMapPclLoader;
-  // gridMapPclLoader.loadParameters(gm::getParameterPath());
-  // gridMapPclLoader.loadCloudFromMessage(pathToCloud);
+//   // grid_map::GridMapPclLoader gridMapPclLoader;
+//   // gridMapPclLoader.loadParameters(gm::getParameterPath());
+//   // gridMapPclLoader.loadCloudFromMessage(pathToCloud);
 
-  // gm::processPointcloud(&gridMapPclLoader, nh);
+//   // gm::processPointcloud(&gridMapPclLoader, nh);
 
-  // grid_map::GridMap gridMap = gridMapPclLoader.getGridMap();
-  // gridMap.setFrameId(gm::getMapFrame(nh));
+//   // grid_map::GridMap gridMap = gridMapPclLoader.getGridMap();
+//   // gridMap.setFrameId(gm::getMapFrame(nh));
 
-  // // publish grid map
-  // grid_map_msgs::GridMap msg;
-  // grid_map::GridMapRosConverter::toMessage(gridMap, msg);
-  // pub.publish(msg);
-}
+//   // // publish grid map
+//   // grid_map_msgs::GridMap msg;
+//   // grid_map::GridMapRosConverter::toMessage(gridMap, msg);
+//   // pub.publish(msg);
+// }
 
 int main(int argc, char** argv) {
   ROS_INFO("Launched PointCloud2_to_GridMap_msg_node");
 
   ros::init(argc, argv, "PointCloud2_to_GridMap_msg_node");
-  ros::NodeHandle nh("~");
-  gm::setVerbosityLevelToDebugIfFlagSet(nh);
+  ros::NodeHandle nodeHandle("~");
+  gm::setVerbosityLevelToDebugIfFlagSet(nodeHandle);
 
   // // Publisher
   // std::string gridMapTopic;
@@ -73,8 +76,6 @@ int main(int argc, char** argv) {
   // std::string pointCloudTopic;
   // nh.param<std::string>("point_cloud_topic", pointCloudTopic, "point_cloud");
   // ros::Subscriber sub = nh.subscribe(pointCloudTopic, 1, callback);
-
-  ros::NodeHandle nodeHandle("~");
 
   PointCloud2ToGridMapMsgNode node = PointCloud2ToGridMapMsgNode(nodeHandle);
 
