ed219dd92c25879fedbd729250ec650d446da719
Ryan Friedman <ryanfriedman5410+github@gmail.com>
2024-07-28 16:28:27 -0600

    Fix linters
    Signed-off-by: Ryan Friedman <ryanfriedman5410+github@gmail.com>

--- grid_map_ros/src/GridMapRosConverter.cpp
+++ grid_map_ros/src/GridMapRosConverter.cpp
@@ -721,10 +721,11 @@ bool GridMapRosConverter::loadFromBag(
   grid_map_msgs::msg::GridMap extracted_gridmap_msg;
   rclcpp::Serialization<grid_map_msgs::msg::GridMap> serialization;
 
-  // Validate the received bag topic exists and is of the correct type to prevent later serialization exception.
+  // Validate the received bag topic exists and
+  // is of the correct type to prevent later serialization exception.
   const auto topic_metadata = reader.get_all_topics_and_types();
   bool topic_is_correct_type = false;
-  for (const auto & m: topic_metadata) {
+  for (const auto & m : topic_metadata) {
     if (m.name == topic && m.type == "grid_map_msgs/msg/GridMap") {
       topic_is_correct_type = true;
     }
