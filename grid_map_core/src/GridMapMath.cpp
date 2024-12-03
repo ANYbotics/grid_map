aalbaali/grid_map_core

Merge branch 'fix/grid_map_core_remove_compiler_warnings' into 'master'
[grid_map_core] Remove compiler warnings

GitOrigin-RevId: 593af35e98d1d24b28f8aaedbc18d96e99f861cf

src/GridMapMath.cpp
@@ -517,21 +517,6 @@ Index getIndexFromLinearIndex(const size_t linearIndex, const Size& bufferSize, 
   return Index((int)linearIndex / bufferSize(1), (int)linearIndex % bufferSize(1));
 }
 
-void getIndicesForRegion(const Index& regionIndex, const Size& regionSize,
-                         std::vector<Index> indices)
-{
-//  for (int i = line.index_; col < line.endIndex(); col++) {
-//    for (int i = 0; i < getSize()(0); i++) {
-//
-//    }
-//  }
-}
-
-void getIndicesForRegions(const std::vector<Index>& regionIndeces, const Size& regionSizes,
-                          std::vector<Index> indices)
-{
-}
-
 bool colorValueToVector(const unsigned long& colorValue, Eigen::Vector3i& colorVector)
 {
   colorVector(0) = (colorValue >> 16) & 0x0000ff;
@@ -564,9 +549,9 @@ bool colorVectorToValue(const Eigen::Vector3i& colorVector, unsigned long& color
 
 void colorVectorToValue(const Eigen::Vector3i& colorVector, float& colorValue)
 {
-  unsigned long color = (colorVector(0) << 16) + (colorVector(1) << 8) + colorVector(2);
-  // cppcheck-suppress invalidPointerCast
-  colorValue = *reinterpret_cast<float*>(&color);
+  Color colors;
+  colors.longColor = (colorVector(0) << 16) + (colorVector(1) << 8) + colorVector(2);
+  colorValue = colors.floatColor;
 }
 
 void colorVectorToValue(const Eigen::Vector3f& colorVector, float& colorValue)

ANYbotics/grid_map

grid_map_core/src/GridMapMath.cpp
@@ -607,23 +607,6 @@ Index getIndexFromLinearIndex(
       1), static_cast<int>(linearIndex) % bufferSize(1));
 }
 
-// void getIndicesForRegion(
-//   const Index & regionIndex, const Size & regionSize,
-//   std::vector<Index> indices)
-// {
-// //  for (int i = line.index_; col < line.endIndex(); col++) {
-// //    for (int i = 0; i < getSize()(0); i++) {
-// //
-// //    }
-// //  }
-// }
-
-// void getIndicesForRegions(
-//   const std::vector<Index> & regionIndeces, const Size & regionSizes,
-//   std::vector<Index> indices)
-// {
-// }
-
 bool colorValueToVector(const uint64_t & colorValue, Eigen::Vector3i & colorVector)
 {
   colorVector(0) = (colorValue >> 16) & 0x0000ff;
@@ -657,9 +640,9 @@ bool colorVectorToValue(const Eigen::Vector3i & colorVector, uint64_t & colorVal
 
 void colorVectorToValue(const Eigen::Vector3i & colorVector, float & colorValue)
 {
-  uint64_t color = (colorVector(0) << 16) + (colorVector(1) << 8) + colorVector(2);
-  float * temp = reinterpret_cast<float *>(&color);
-  colorValue = *temp;
+  Color colors;
+  colors.intColor = (colorVector(0) << 16) + (colorVector(1) << 8) + colorVector(2);
+  colorValue = colors.floatColor;
 }
 
 void colorVectorToValue(const Eigen::Vector3f & colorVector, float & colorValue)